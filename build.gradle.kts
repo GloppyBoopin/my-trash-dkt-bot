4e04bfdf58a493d8a7d3505bb41e88a15118728e
AABalandin <aabalandin@beeline.kz>
2022-06-12 14:46:11 +0600

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,30 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    kotlin("jvm") version "1.5.10"
-    application
-}
-
 group = "me.couca"
 version = "1.0-SNAPSHOT"
+val kotestVersion = "5.3.0"
+val discordKtVersion = "0.23.0-SNAPSHOT"
+
+plugins {
+  java
+  kotlin("jvm") version "1.5.10"
+}
 
 repositories {
-    mavenCentral()
+  mavenCentral()
+  maven("https://oss.sonatype.org/content/repositories/snapshots/")
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+  implementation("me.jakejmattson:DiscordKt:$discordKtVersion")
+  testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+  testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
 }
 
 tasks.test {
-    useJUnitPlatform()
+  useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
-}
-
-application {
-    mainClass.set("MainKt")
+  kotlinOptions.jvmTarget = "11"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,1 @@
-
 rootProject.name = "my-trash-dkt-bot"
-
